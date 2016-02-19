4c61b8efd6dc4d31b3a35e4d345ce82a4257e647
zztop <zaryab.ahmad@gmail.com>
2016-02-18 23:36:29 -0500

    prevent rebuils if file hasnt changed

--- redo-ifchange.sh
+++ redo-ifchange.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/sh
 
 echo $REDO_TARGET $@ 1>&2
+
+for dep in $@; do
+	mkdir -p ".redo/$REDO_TARGET"
+	md5sum $dep | awk '{print $1}'> ".redo/$REDO_TARGET/$dep" 
+done
--- redo.hs
+++ redo.hs
@@ -1,35 +1,43 @@
-import System.Environment (getArgs,getEnvironment)
-import System.Process (createProcess, waitForProcess, shell,CreateProcess(..))
-import System.Directory (renameFile,removeFile,doesFileExist)
-import System.FilePath (replaceBaseName,hasExtension,takeBaseName)
-import System.Exit (ExitCode(..))
-import System.IO (hPutStrLn,stderr)
-import Control.Monad (filterM,liftM)
-import Data.Maybe (listToMaybe)
-import Debug.Trace (trace)
+import           Control.Monad        (filterM, liftM,unless)
+import           Control.Monad.Loops  (allM)
+import qualified Data.ByteString.Lazy as LB
+import           Data.Digest.Pure.MD5 (md5)
+import           Data.Maybe           (listToMaybe)
+import           Debug.Trace          (trace)
+import           System.Directory     (doesFileExist, getDirectoryContents,
+                                       removeFile, renameFile)
+import           System.Environment   (getArgs, getEnvironment)
+import           System.Exit          (ExitCode (..))
+import           System.FilePath      (hasExtension, replaceBaseName,
+                                       takeBaseName, (</>))
+import           System.IO            (hPutStrLn, stderr,hGetLine,withFile,IOMode (..))
+import           System.Process       (CreateProcess (..), createProcess, shell,
+                                       waitForProcess)
 
-
+ 
 main :: IO ()
 main =  getArgs >>= mapM_ redo
 
 redo :: String -> IO ()
 redo target = do
   let tmp = target ++ "---redoing"
-  targetPath <-redoPath target
-  case targetPath of 
-  	Nothing -> error "files not present"
-  	Just targetPath -> do 
-
-  						putStrLn targetPath
-  						oldEnv <- getEnvironment
-					  	(_, _, _, ph) <- createProcess (shell  $ cmd targetPath){env = Just ( ("REDO_TARGET", target) : oldEnv)}  
-					  	exit <- waitForProcess ph
-					  	case exit of
-						   	ExitSuccess ->  renameFile tmp target
-						   	ExitFailure code -> do 
-					   						hPutStrLn stderr $ "Redo script exited with non zero code" ++ show code 
-					   						removeFile tmp
-					   	where cmd targetPath = trace' $ "sh " ++ targetPath ++ " 0 " ++ " " ++ takeBaseName target ++ " " ++ tmp ++ " > " ++ tmp
+  hasUpdated <- hasFilesUpdated target
+  unless hasUpdated $
+  	do 
+  		targetPath <-redoPath target
+  		case targetPath of
+	  		Nothing -> error "files not present"
+	  		Just targetPath -> do
+	  						putStrLn targetPath
+	  						oldEnv <- getEnvironment
+						  	(_, _, _, ph) <- createProcess (shell  $ cmd targetPath){env = Just ( ("REDO_TARGET", target) : oldEnv)}
+						  	exit <- waitForProcess ph
+						  	case exit of
+							   	ExitSuccess ->  renameFile tmp target
+							   	ExitFailure code -> do
+						   						hPutStrLn stderr $ "Redo script exited with non zero code" ++ show code
+						   						removeFile tmp
+						   	where cmd targetPath = trace' $ "sh " ++ targetPath ++ " 0 " ++ " " ++ takeBaseName target ++ " " ++ tmp ++ " > " ++ tmp
 
 redoPath :: FilePath -> IO (Maybe FilePath)
 
@@ -37,4 +45,16 @@ redoPath target = liftM listToMaybe $ filterM doesFileExist filesToProcess
 					where filesToProcess = (target ++ ".do") : [replaceBaseName target "default" ++ ".do" | hasExtension target]
 
 trace' arg = trace arg arg
-					
+
+hasFilesUpdated :: String -> IO Bool
+hasFilesUpdated dep = do
+	files <- liftM  (filter  (`notElem` [".",".." ]))  (getDirectoryContents  (".redo" </> dep))
+	(\x -> hasFileUpdated (".redo" </> dep </> x) x) `allM` files
+hasFileUpdated :: FilePath -> FilePath -> IO Bool
+hasFileUpdated dep target  = do
+	oldMD5 <- withFile dep ReadMode hGetLine
+	newMD5 <- liftM md5 (LB.readFile target)
+	return $ oldMD5 == show newMD5
+
+
+
