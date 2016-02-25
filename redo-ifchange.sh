272e30f4a4b0828f06df67a8f1aea2a21791ea4f
zztop <zaryab.ahmad@gmail.com>
2016-02-25 00:46:14 -0500

    fixing matching of md5

--- redo-ifchange.sh
+++ redo-ifchange.sh
@@ -3,6 +3,5 @@
 echo $REDO_TARGET $@ 1>&2
 
 for dep in $@; do
-	mkdir -p ".redo/$REDO_TARGET"
 	md5sum $dep | awk '{print $1}'> ".redo/$REDO_TARGET/$dep" 
 done
--- redo.hs
+++ redo.hs
@@ -5,7 +5,7 @@ import           Data.Digest.Pure.MD5 (md5)
 import           Data.Maybe           (listToMaybe)
 import           Debug.Trace          (trace)
 import           System.Directory     (doesFileExist, getDirectoryContents,
-                                       removeFile, renameFile)
+                                       removeFile, renameFile,removeDirectoryRecursive,createDirectoryIfMissing)
 import           System.Environment   (getArgs, getEnvironment)
 import           System.Exit          (ExitCode (..))
 import           System.FilePath      (hasExtension, replaceBaseName,
@@ -18,26 +18,33 @@ import           System.Process       (CreateProcess (..), createProcess, shell,
 main :: IO ()
 main =  getArgs >>= mapM_ redo
 
+
 redo :: String -> IO ()
-redo target = do
-  let tmp = target ++ "---redoing"
-  hasUpdated <- hasFilesUpdated target
-  unless hasUpdated $
-  	do 
-  		targetPath <-redoPath target
-  		case targetPath of
-	  		Nothing -> error "files not present"
-	  		Just targetPath -> do
-	  						putStrLn targetPath
-	  						oldEnv <- getEnvironment
-						  	(_, _, _, ph) <- createProcess (shell  $ cmd targetPath){env = Just ( ("REDO_TARGET", target) : oldEnv)}
-						  	exit <- waitForProcess ph
-						  	case exit of
-							   	ExitSuccess ->  renameFile tmp target
-							   	ExitFailure code -> do
-						   						hPutStrLn stderr $ "Redo script exited with non zero code" ++ show code
-						   						removeFile tmp
-						   	where cmd targetPath = trace' $ "sh " ++ targetPath ++ " 0 " ++ " " ++ takeBaseName target ++ " " ++ tmp ++ " > " ++ tmp
+redo target = do 
+	removeDirectoryRecursive depsDir
+	createDirectoryIfMissing True depsDir
+	targetMd5 <- md5' target
+	writeFile (depsDir </> target) targetMd5
+	let tmp = target ++ "---redoing"
+	hasUpdated <-  hasFilesUpdated target
+	unless hasUpdated $
+		do 
+			targetPath <-redoPath target
+			case targetPath of
+		  		Nothing -> error "files not present"
+		  		Just targetPath -> do
+		  						putStrLn targetPath
+		  						oldEnv <- getEnvironment
+							  	(_, _, _, ph) <- createProcess (shell  $ cmd targetPath){env = Just ( ("REDO_TARGET", target) : oldEnv)}
+							  	exit <- waitForProcess ph
+							  	case exit of
+								   	ExitSuccess ->  renameFile tmp target
+								   	ExitFailure code -> do
+							   						hPutStrLn stderr $ "Redo script exited with non zero code" ++ show code
+							   						removeFile tmp
+							   	where cmd targetPath = trace' $ "sh " ++ targetPath ++ " 0 " ++ " " ++ takeBaseName target ++ " " ++ tmp ++ " > " ++ tmp
+	where depsDir = ".redo" </> target
+
 
 redoPath :: FilePath -> IO (Maybe FilePath)
 
@@ -53,8 +60,11 @@ hasFilesUpdated dep = do
 hasFileUpdated :: FilePath -> FilePath -> IO Bool
 hasFileUpdated dep target  = do
 	oldMD5 <- withFile dep ReadMode hGetLine
-	newMD5 <- liftM md5 (LB.readFile target)
-	return $ oldMD5 == show newMD5
+	newMD5 <- md5' target
+	return $ oldMD5 == newMD5
 
 
-
+md5' :: FilePath -> IO String
+md5' path = do
+	newMD5 <- liftM md5 (LB.readFile path)
+	return $ show newMD5
